4fb91c4b207c1d8741fa8565576e1b0245a92303
Htet Aung Lwin <hurricane2922@gmail.com>
2023-12-19 22:04:14 +0630

    Add anime.js

---
+++ js/anime.js
@@ -0,0 +1,26 @@
+const arc = [
+  {
+    img: "Cursed Child Arc",
+    number: "one",
+  },
+  {
+    img: "Fearsome Womb Arc",
+    number: "two",
+  },
+  {
+    img: "Kyoto Goodwill Event Arc",
+    number: "three",
+  },
+  {
+    img: "Hidden Inventory Arc",
+    number: "four",
+  },
+  {
+    img: "Shibuya Incident Arc",
+    number: "five",
+  },
+];
+
+let animeImg = document.querySelector(".anime-poster-img");
+
+console.log(animeImg.src);
