4385762eddd58f17437e250a4ce64f4bebebea57
Htet Aung Lwin <htetaunglwin.c@non@gmail.com>
2025-01-03 12:06:29 +0630

    update manga volumes and section styles for small screens

--- js/manga.js
+++ js/manga.js
@@ -2,12 +2,12 @@ let group = document.querySelector(".manga-group");
 
 function addManga() {
   let mainHTML = "";
-  for (let i = 0; i < 26; i++) {
+  for (let i = 0; i < 31; i++) {
     let html = ` <div><img src="img/manga vol/${i}.jpg" alt="" /></div>`;
 
     mainHTML += html;
   }
-  mainHTML += "<div><p>This is the latest chapter. Ongoing....</p></div>";
+  mainHTML += "<div><p>This is the latest chapter.</p></div>";
 
   group.innerHTML = mainHTML;
 }
